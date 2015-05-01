9f947f73db4fcf9abc51b3561965750c98035d54
zsx <thinkernel@gmail.com>
2015-05-02 02:43:53 +0800

    Hide Gerrit's 8080 port.

--- createGerrit.sh
+++ createGerrit.sh
@@ -9,8 +9,8 @@ PG_GERRIT_NAME=${PG_GERRIT_NAME:-pg-gerrit}
 GERRIT_IMAGE_NAME=${GERRIT_IMAGE_NAME:-openfrontier/gerrit}
 LOCAL_VOLUME=~/gerrit_volume${SUFFIX}
 
-docker run --name $PG_GERRIT_NAME -p 5432:5432 -e POSTGRES_USER=gerrit2 -e POSTGRES_PASSWORD=gerrit -e POSTGRES_DB=reviewdb -d postgres
+docker run --name $PG_GERRIT_NAME -P -e POSTGRES_USER=gerrit2 -e POSTGRES_PASSWORD=gerrit -e POSTGRES_DB=reviewdb -d postgres
 sleep 5
 mkdir -p "${LOCAL_VOLUME}"
-docker run --name ${GERRIT_NAME} --link ${PG_GERRIT_NAME}:db -p 8080:8080 -p 29418:29418 -v ${LOCAL_VOLUME}:/var/gerrit/review_site -e WEBURL=${GERRIT_WEBURL} -e HTTPD_LISTENURL=${HTTPD_LISTENURL} -e DATABASE_TYPE=postgresql -e AUTH_TYPE=LDAP -e LDAP_SERVER=${LDAP_SERVER} -e LDAP_ACCOUNTBASE=${LDAP_ACCOUNTBASE} -d ${GERRIT_IMAGE_NAME}
+docker run --name ${GERRIT_NAME} --link ${PG_GERRIT_NAME}:db -p 29418:29418 -v ${LOCAL_VOLUME}:/var/gerrit/review_site -e WEBURL=${GERRIT_WEBURL} -e HTTPD_LISTENURL=${HTTPD_LISTENURL} -e DATABASE_TYPE=postgresql -e AUTH_TYPE=LDAP -e LDAP_SERVER=${LDAP_SERVER} -e LDAP_ACCOUNTBASE=${LDAP_ACCOUNTBASE} -d ${GERRIT_IMAGE_NAME}
 
