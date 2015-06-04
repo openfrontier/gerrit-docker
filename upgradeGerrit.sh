dd9957cd878a0c704b6f1a9375246eeab112b10a
zsx <thinkernel@gmail.com>
2015-06-04 06:11:40 +0000

    Add upgrade script.

---
+++ upgradeGerrit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+GERRIT_WEBURL=${GERRIT_WEBURL:-$1}
+LDAP_SERVER=${LDAP_SERVER:-$2}
+LDAP_ACCOUNTBASE=${LDAP_ACCOUNTBASE:-$3}
+HTTPD_LISTENURL=${HTTPD_LISTENURL:-http://*:8080}
+GERRIT_NAME=${GERRIT_NAME:-gerrit}
+GERRIT_VOLUME=${GERRIT_VOLUME:-gerrit-volume}
+PG_GERRIT_NAME=${PG_GERRIT_NAME:-pg-gerrit}
+GERRIT_IMAGE_NAME=${GERRIT_IMAGE_NAME:-openfrontier/gerrit}
+
+# Stop and Delete gerrit container.
+docker stop ${GERRIT_NAME}
+docker rm -v ${GERRIT_NAME}
+
+# Start Gerrit.
+docker run \
+--name ${GERRIT_NAME} \
+--link ${PG_GERRIT_NAME}:db \
+-p 29418:29418 \
+--volumes-from ${GERRIT_VOLUME} \
+-e WEBURL=${GERRIT_WEBURL} \
+-e HTTPD_LISTENURL=${HTTPD_LISTENURL} \
+-e DATABASE_TYPE=postgresql \
+-e AUTH_TYPE=LDAP \
+-e LDAP_SERVER=${LDAP_SERVER} \
+-e LDAP_ACCOUNTBASE=${LDAP_ACCOUNTBASE} \
+-d ${GERRIT_IMAGE_NAME}
+
