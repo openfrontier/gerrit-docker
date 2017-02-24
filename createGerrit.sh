e5feef6fe1ab85dd81fd63bdc84240c76da3bcaa
thinkernel <thinkernel@gmail.com>
2017-02-24 08:00:18 +0000

    add replication plugin by default

--- createGerrit.sh
+++ createGerrit.sh
@@ -58,7 +58,7 @@ docker run \
 -e SMTP_USER=${SMTP_USER} \
 -e SMTP_PASS=${SMTP_PASS} \
 -e USER_EMAIL=${USER_EMAIL} \
--e GERRIT_INIT_ARGS='--install-plugin=download-commands' \
+-e GERRIT_INIT_ARGS='--install-plugin=download-commands --install-plugin=replication' \
 -e INITIAL_ADMIN_USER=${GERRIT_ADMIN_UID} \
 -e INITIAL_ADMIN_PASSWORD=${GERRIT_ADMIN_PWD} \
 -e JENKINS_HOST=jenkins \
