63e55595fd4a0494b527cf5391d1335be5350dd4
thinkernel <thinkernel@gmail.com>
2017-08-22 06:55:11 +0000

    Waiting db before startup

---
+++ gerrit-wait-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wait_for_database() {
+  echo "Waiting for database connection $1:$2 ..."
+  until nc -z $1 $2; do
+    sleep 1
+  done
+}
+
+case "${DATABASE_TYPE}" in
+  postgresql) wait_for_database ${DB_PORT_5432_TCP_ADDR} ${DB_PORT_5432_TCP_PORT}
+              ;;
+  mysql)      wait_for_database ${DB_PORT_3306_TCP_ADDR} ${DB_PORT_3306_TCP_PORT}
+              ;;
+  *)
+              ;;
+esac
