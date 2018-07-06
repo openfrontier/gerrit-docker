0df2f1deaafc311f9c55430e5b9f1510078dc045
berg207 <justuslasse.berger@bertelsmann.de>
2018-07-06 11:03:01 +0200

    Make local Gerrit URL configurable via environment

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM openfrontier/gerrit:latest
 
 MAINTAINER zsx <thinkernel@gmail.com>
 
+ENV INITIAL_ADMIN_USER      admin
+ENV INITIAL_ADMIN_PASSWORD  admin
+ENV GERRIT_LOCAL_URL        http://localhost:8080/gerrit
+
 COPY gerrit-create-user.sh      /usr/local/bin/gerrit-create-user.sh
 COPY gerrit-upload-ssh-key.sh   /usr/local/bin/gerrit-upload-ssh-key.sh
 COPY gerrit-init.nohup          /docker-entrypoint-init.d/gerrit-init.nohup
--- gerrit-upload-ssh-key.sh
+++ gerrit-upload-ssh-key.sh
@@ -9,11 +9,14 @@ usage() {
     exit 1
 }
 
+# Constants configurable via environment
+GERRIT_URL=${GERRIT_LOCAL_URL:-'http://localhost:8080/gerrit'}
+
 # Constants
 SLEEP_TIME=10
 
 while getopts "c:p:A:P:k:u:" opt; do
-  case $opt in
+  case "${opt}" in
     c)
       host=${OPTARG}
       ;;
@@ -58,7 +61,7 @@ echo "Retrieving value: ${key}"
 ssh_key=$(curl --silent --request GET --user ${username}:${password} "http://${host}:${port}/jenkins/userContent/${key}")
 
 echo "Checking if \"${user}\" exists"
-if curl --location --output /dev/null --silent --write-out "%{http_code}\\n" "http://localhost:8080/gerrit/accounts/${user}" | grep "404" &> /dev/null; then
+if curl --location --output /dev/null --silent --write-out "%{http_code}\\n" "${GERRIT_URL}/accounts/${user}" | grep "404" &> /dev/null; then
     echo "User does not exist: ${user}"
     exit 1
 fi
@@ -67,7 +70,7 @@ echo "* Verify public-key existence"
 # Download the stored key and decode from to UTF-8
 # Using echo -e the -n switch from echo allows to remove the trailing \n that echo would add
 # The decode part is necessary as Gerrit correctly encode the SSH key and as a result = sign is converted to \u003d
-stored_key=$(echo -e $(curl --user ${username}:${password} --silent http://localhost:8080/gerrit/a/accounts/${user}/sshkeys | grep "ssh_public_key" | awk '{split($0, a, ": "); print a[2]}' | sed 's/[",]//g'))
+stored_key=$(echo -e $(curl --user ${username}:${password} --silent "${GERRIT_URL}/a/accounts/${user}/sshkeys" | grep "ssh_public_key" | awk '{split($0, a, ": "); print a[2]}' | sed 's/[",]//g'))
 if [[ "$stored_key" == "$ssh_key" ]]; then
   echo "* Stored key is the same as downloaded, skipping it ..."
   exit 0
@@ -76,7 +79,7 @@ else
 fi
 
 echo "Uploading public-key to Gerrit user \"${user}\""
-ret=$(curl --request POST --user "${username}:${password}" --data "${ssh_key}" --output /dev/null --silent --write-out "%{http_code}" "http://localhost:8080/gerrit/a/accounts/${user}/sshkeys")
+ret=$(curl --request POST --user "${username}:${password}" --data "${ssh_key}" --output /dev/null --silent --write-out "%{http_code}" "${GERRIT_URL}/a/accounts/${user}/sshkeys")
 if [[ ${ret} -eq 201 ]]; then
   echo "Public-key was uploaded"
 else
