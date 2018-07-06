98cd2483bada51b7c4894cfa413c5b0b60938ef2
berg207 <justuslasse.berger@bertelsmann.de>
2018-07-06 11:11:51 +0200

    Make remote Jenkins URL configurable via environment

--- gerrit-upload-ssh-key.sh
+++ gerrit-upload-ssh-key.sh
@@ -5,24 +5,19 @@ set -e
 # Usage
 usage() {
     echo "Usage:"
-    echo "    ${0} -c <JENKINS_HOSTS> -p <JENKINS_PORT> -A <username> -P <password> -k <KEY_FOR_USER> -u <FOR_USER>"
+    echo "    ${0} -A <username> -P <password> -k <KEY_FOR_USER> -u <FOR_USER>"
     exit 1
 }
 
 # Constants configurable via environment
 GERRIT_URL=${GERRIT_LOCAL_URL:-'http://localhost:8080/gerrit'}
+JENKINS_URL=${JENKINS_REMOTE_URL:-'http://jenkins:8080/jenkins'}
 
 # Constants
 SLEEP_TIME=10
 
-while getopts "c:p:A:P:k:u:" opt; do
+while getopts "A:P:k:u:" opt; do
   case "${opt}" in
-    c)
-      host=${OPTARG}
-      ;;
-    p)
-      port=${OPTARG}
-      ;;
     A)
       username=${OPTARG}
       ;;
@@ -42,7 +37,7 @@ while getopts "c:p:A:P:k:u:" opt; do
   esac
 done
 
-if [ -z "${host}" ] || [ -z "${port}" ] || [ -z "${username}" ] || [ -z "${password}" ] || [ -z "${key}" ] || [ -z "${user}" ]; then
+if [ -z "${username}" ] || [ -z "${password}" ] || [ -z "${key}" ] || [ -z "${user}" ]; then
     echo "Parameters missing"
     usage
 fi
@@ -51,14 +46,14 @@ key=$(echo -e "${key}" | sed 's/ /%20/g')
 user=$(echo -e "${user}" | sed 's/ /%20/g')
 
 echo "Testing Jenkins Connection & Key Presence"
-until curl --location --user ${username}:${password} --output /dev/null --silent --write-out "%{http_code}\\n" "http://${host}:${port}/jenkins/userContent/${key}" | grep "200" &> /dev/null
+until curl --location --user ${username}:${password} --output /dev/null --silent --write-out "%{http_code}\\n" "${JENKINS_URL}/userContent/${key}" | grep "200" &> /dev/null
 do
     echo "Jenkins or key unavailable, sleeping for ${SLEEP_TIME}"
     sleep "${SLEEP_TIME}"
 done
 
 echo "Retrieving value: ${key}"
-ssh_key=$(curl --silent --request GET --user ${username}:${password} "http://${host}:${port}/jenkins/userContent/${key}")
+ssh_key=$(curl --silent --request GET --user ${username}:${password} "${JENKINS_URL}/userContent/${key}")
 
 echo "Checking if \"${user}\" exists"
 if curl --location --output /dev/null --silent --write-out "%{http_code}\\n" "${GERRIT_URL}/accounts/${user}" | grep "404" &> /dev/null; then
